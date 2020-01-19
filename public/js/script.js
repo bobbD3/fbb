9a2379307865056b7b1491a4f91937e23b03cdba
Bozhidar Donchev <bozhidar.donchev@bg.ibm.com>
2020-01-19 17:54:07 +0200

    blogIMG

--- public/js/script.js
+++ public/js/script.js
@@ -131,7 +131,17 @@ $(function () {
     new WOW().init();
 });
 
+/* Cookies*/
 
+    if (localStorage.getItem('cookieSeen') != 'shown') {
+        $('.cookie-banner').delay(2000).fadeIn();
+        localStorage.setItem('cookieSeen','shown')
+      };
+
+
+$('.close').click(function() {
+    $('.cookie-banner').fadeOut();
+  })
 /* =========================================
                 Hover 
 ============================================ */
